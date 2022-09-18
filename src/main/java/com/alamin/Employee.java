1dd1e56e698c7780830ea5adbf972962ff8458ce
AlaminRonya <hprony12@gmail.com>
2022-09-19 01:14:22 +0600

    Consumer method is used to primitive data type and object type

--- src/main/java/com/alamin/Employee.java
+++ src/main/java/com/alamin/Employee.java
@@ -1,2 +1,31 @@
-package com.alamin;public class Employee {
+package com.alamin;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Employee {
+    private int id;
+    private String name;
+    private String address;
+    private double salary;
+
+    public static List<Employee> employees = List.of(
+            new Employee(1, "Saima","Cox-Bazar", 4000),
+            new Employee(2,"Md. Al Amin","Pabna",5000),
+            new Employee(2,"Sabbir","Pabna",15000),
+            new Employee(2,"Masud","Pabna",20000),
+            new Employee(2,"Jahangir","Dhaka",25000),
+            new Employee(2,"Manik","Barisal",25000),
+            new Employee(2,"Salman","Dhaka",15000),
+            new Employee(2,"Ashik","Dhaka",35000)
+    );
+
 }
--- src/main/java/com/alamin/consumer/ConsumerInterface.java
+++ src/main/java/com/alamin/consumer/ConsumerInterface.java
@@ -1,2 +1,37 @@
-package com.alamin.consumer;public class ConsumerInterface {
+package com.alamin.consumer;
+
+import com.alamin.Employee;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class ConsumerInterface {
+
+    // TODO: Consumer<T> is input value but not return value (i.e print)
+    // TODO: When using andThen, must be type same
+
+    static Consumer<String> toUpperCase = s-> System.out.println(s.toUpperCase());
+    static Consumer<String> toLowerCase = s-> System.out.println(s.toLowerCase());
+
+    static Predicate<Employee> salaryRange = employee -> employee.getSalary()>15000;
+
+    static Consumer<List<Employee>> allEmp = System.out::println;
+    static Consumer<List<Employee>> empName = emp -> emp.forEach(e-> System.out.println(e.getName()));
+    static Consumer<List<Employee>> empAddress = emp -> emp.forEach(e-> System.out.println(e.getAddress().toUpperCase()));
+    static Consumer<List<Employee>> empSalaryRange = emp -> emp.stream().filter(salaryRange).forEach(System.out::println);
+    public static void main(String[] args) {
+        toUpperCase.accept("Java Programmer");
+        toLowerCase.accept("Java Programmer");
+        toUpperCase.andThen(toLowerCase).accept("Hi, I'm spring & spring-boot developer.");
+
+        System.out.println("=================Individual ====================");
+        allEmp.accept(Employee.employees);
+        empName.accept(Employee.employees);
+        empAddress.accept(Employee.employees);
+        empSalaryRange.accept(Employee.employees);
+        System.out.println("=================Combine all method======================");
+        allEmp.andThen(empName).andThen(empAddress).andThen(empSalaryRange).accept(Employee.employees);
+
+    }
 }
