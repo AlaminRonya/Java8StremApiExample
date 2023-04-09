89f027d647be9a56eff20d06d1def8a58209f085
AlaminRonya <hprony12@gmail.com>
2023-04-09 12:28:59 +0600

    Code basic change

--- src/main/java/com/alamin/ComparableInterface/AgeComparator.java
+++ src/main/java/com/alamin/ComparableInterface/AgeComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class AgeComparator implements Comparator<Student> {
     @Override
     public int compare(Student o1, Student o2) {
-        return o1.getAge() > o2.getAge() ? 1 : o1.getAge() < o2.getAge() ? -1 : 0;
+        return Integer.compare(o1.getAge(), o2.getAge());
     }
 
     @Override
--- src/main/java/com/alamin/fibonacci/Fibonacci.java
+++ src/main/java/com/alamin/fibonacci/Fibonacci.java
@@ -25,7 +25,7 @@ public class Fibonacci {
         System.out.println(s);
 
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         fibonacciSeries(10);
         checkFibonacciNumber(50);
     }
