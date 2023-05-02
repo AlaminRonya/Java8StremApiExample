e08ed7346138fc630e9fac4b7b766c0d16423682
AlaminRonya <hprony12@gmail.com>
2023-05-02 09:10:40 +0600

    Bug of hashmap test

--- src/main/java/com/alamin/map_work/BugCode.java
+++ src/main/java/com/alamin/map_work/BugCode.java
@@ -1,5 +1,8 @@
 package com.alamin.map_work;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class BugCode {
     public static void main(String[] args) {
         String bug1 = new String("FB");
@@ -9,5 +12,25 @@ public class BugCode {
         System.out.println((bug1.hashCode() == bug2.hashCode())
                 +" : "+(bug3.hashCode() == bug4.hashCode())
                 +" : "+(bug2.hashCode() == bug4.hashCode()));
+
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add(bug1);
+        System.out.println(hashSet);
+        hashSet.add(bug2);
+        System.out.println(hashSet);
+        hashSet.add(bug2);
+        hashSet.add(bug3);
+        System.out.println(hashSet);
+
+
+        HashMap<String,Integer> hashMap = new HashMap<>();
+        hashMap.put(bug1, 10);
+        System.out.println(hashMap);
+        hashMap.put(bug2, 20);
+        System.out.println(hashMap);
+        hashMap.put(bug3, 30);
+        hashMap.put(bug4, 20);
+
+        System.out.println(hashMap);
     }
 }
