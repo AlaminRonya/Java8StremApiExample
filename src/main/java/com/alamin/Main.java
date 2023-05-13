ece504cdb334de08d11a58b3850fe51c998e686f
AlaminRonya <hprony12@gmail.com>
2023-05-13 10:31:15 +0600

    Text design and Animation

--- src/main/java/com/alamin/Main.java
+++ src/main/java/com/alamin/Main.java
@@ -1,7 +1,14 @@
 package com.alamin;
 
 public class Main {
+    public static void fun(long a, int b){
+        System.out.println("long, double");
+    }
+    public static void fun(int a, float b){
+        System.out.println("int, float");
+    }
     public static void main(String[] args) {
-        System.out.println("I'm Md. Al Amin\nwebclubrony@gmail.com");
+//        System.out.println("I'm Md. Al Amin\nwebclubrony@gmail.com");
+//        fun((byte)10, 20);
     }
 }
