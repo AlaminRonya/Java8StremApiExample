778b8237c725a55584f497bae4c96ace7ad4cefa
AlaminRonya <hprony12@gmail.com>
2023-08-07 21:09:19 +0600

    Duplicate value checking change code

--- src/main/java/com/alamin/esay_java/ContainsDuplicate.java
+++ src/main/java/com/alamin/esay_java/ContainsDuplicate.java
@@ -1,22 +1,53 @@
 package com.alamin.esay_java;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4};
+        int[] nums = {0,4,5,0,3,6};
         System.out.println(new ContainsDuplicate().containsDuplicate(nums));
 
     }
-    public boolean containsDuplicate(int[] nums) {
-        Set<Integer> numSet = new HashSet<>();
-        for (int num : nums) {
-            if (numSet.contains(num)) {
-                return true;
-            }
-            numSet.add(num);
+//    public boolean containsDuplicate(int[] nums) {
+//        Set<Integer> numSet = new HashSet<>();
+//        for (int i = 0; i <= nums.length/2; i++) {
+//            if (numSet.contains(nums[i]) ) {
+//                return true;
+//            }
+//            numSet.add(nums[i]);
+//
+//        }
+//        return false;
+//    }
+public  boolean containsDuplicate(int[] nums) {
+    int n = nums.length;
+    int[] seen = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        if (seen[nums[i]] == 1) {
+            return true;
         }
-        return false;
+        seen[nums[i]] = 1;
     }
+
+    return false;
+}
+
+//    public boolean containsDuplicate(int[] nums) {
+//        List<Integer> bitVector = new ArrayList<>();
+//        for (int num : nums) {
+//            int index = num / 32;
+//            int bit = 1 << (num % 32);
+//
+//            if ((bitVector.get(index) & bit) != 0) {
+//                return true;
+//            }
+//
+////            bitVector.get(index).toSt |= bit;
+//        }
+//        return false;
+//    }
 }
