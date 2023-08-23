d1be51c0439009338d5f67aec7856d643ec59064
AlaminRonya <hprony12@gmail.com>
2023-08-23 10:38:31 +0600

    Is Palindrome

--- src/main/java/com/alamin/sdlc_pro/basic_math/ReverseNumber.java
+++ src/main/java/com/alamin/sdlc_pro/basic_math/ReverseNumber.java
@@ -2,8 +2,9 @@ package com.alamin.sdlc_pro.basic_math;
 
 public class ReverseNumber {
     public static void main(String[] args) {
-        int num = -100;
-        System.out.println(reverseNumber(num));
+        int num = 121;
+//        System.out.println(reverseNumber(num));
+        System.out.println(isPalindrome2(num));
     }
     private static int reverseNumber(int num){
         if (Integer.MIN_VALUE == num || Integer.MAX_VALUE == num){
@@ -21,5 +22,28 @@ public class ReverseNumber {
             throw new RuntimeException("Invalid Number");
         }
         return (int) temp;
+    }
+    public static boolean isPalindrome(int x) {
+        char[] msg = String.valueOf(x).toCharArray();
+        int right = 0;
+        int left = msg.length - 1;
+
+        while (right <= left){
+            if (msg[right] != msg[left]){
+                return false;
+            }
+            right++;
+            left--;
+        }
+        return true;
+    }
+    public static boolean isPalindrome2(int x) {
+        int check = x;
+        int temp = 0;
+        while (x != 0){
+            temp = temp * 10 + x % 10;
+            x /= 10;
+        }
+        return check == temp;
     }
 }
