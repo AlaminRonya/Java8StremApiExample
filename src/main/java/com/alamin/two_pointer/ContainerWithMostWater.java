0714ee74400adfa8e34c7fe5026aa9e8450ef487
AlaminRonya <hprony12@gmail.com>
2023-08-30 21:29:14 +0600

    Contain with most water

---
+++ src/main/java/com/alamin/two_pointer/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+package com.alamin.two_pointer;
+
+public class ContainerWithMostWater {
+    public static void main(String[] args) {
+
+    }
+    public static int maxArea(int[] height) {
+        int n = height.length;
+        int l = 0;
+        int r = n - 1;
+        int maxArea = Integer.MIN_VALUE;
+        while (l < r ){
+            int area = (r - l) * Math.min(height[l], height[r]);
+            maxArea = Math.max(maxArea, area);
+            if (height[l] < height[r]){
+                l++;
+            }else {
+                r--;
+            }
+        }
+        return maxArea;
+    }
+}
