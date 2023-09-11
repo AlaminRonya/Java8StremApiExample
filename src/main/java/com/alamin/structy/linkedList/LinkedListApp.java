24e3a4857e0451705ee5244bde090cf5b399b454
AlaminRonya <hprony12@gmail.com>
2023-09-11 19:38:33 +0600

    Iterative and recursive for listValues, sumList, listFind, reverseList, zipperTwoList

--- src/main/java/com/alamin/structy/linkedList/LinkedListApp.java
+++ src/main/java/com/alamin/structy/linkedList/LinkedListApp.java
@@ -1,5 +1,7 @@
 package com.alamin.structy.linkedList;
 
+import java.util.List;
+
 public class LinkedListApp {
     public static void main(String[] args) {
 //        Node<Integer> a = new Node<>(10);
@@ -26,12 +28,14 @@ public class LinkedListApp {
         root = implementation.insertAtLast(10, null);
         root = implementation.insertAtLast(20, root);
         root = implementation.insertAtLast(30, root);
+        root = implementation.insertAtLast(30, root);
         root = implementation.insertAtFirst(40, root);
         root = implementation.insertAtLast(40, root);
         root = implementation.deleteAtFirst( root);
         root = implementation.deleteAtPosition( root, 3);
-        root = implementation.insertAtFirst(40, root);
-        root = implementation.insertAtPosition(80, root,2);
+//        root = implementation.insertAtFirst(40, root);
+//        root = implementation.insertAtPosition(80, root,2);
+
 
 //        Node<Integer> temp = root;
 //        while (temp != null){
@@ -41,5 +45,34 @@ public class LinkedListApp {
 
         implementation.displayIterator(root);
         implementation.displayRecursively(root);
+        var listValues = implementation.linkedListValuesIterative(root);
+        listValues.forEach(System.out::println);
+        System.out.println("--------------");
+        listValues = implementation.linkedListValuesRecursive(root);
+        listValues.forEach(System.out::println);
+        System.out.println("--------------");
+        System.out.println(implementation.sumListIterative(root));
+        System.out.println("--------------");
+        System.out.println(implementation.sumListRecursive(root));
+        System.out.println("--------------");
+        System.out.println(implementation.linkedListFindIterative(root, 15));
+        System.out.println("--------------");
+        System.out.println(implementation.linkedListFindRecursive(root, 10));
+        System.out.println("-----------------------");
+        implementation.displayIterator(root);
+        Node<Integer> reverseListIterative = implementation.reverseListIterative(root);
+        implementation.displayIterator(reverseListIterative);
+        reverseListIterative = implementation.reverseListRecursive(reverseListIterative);
+        implementation.displayIterator(reverseListIterative);
+
+        Node<Integer> node2 = null;
+        node2 = implementation.insertAtLast(10, null);
+        node2 = implementation.insertAtLast(20, node2);
+        node2 = implementation.insertAtLast(30, node2);
+        node2 = implementation.insertAtLast(40, node2);
+        var zipper1 = implementation.zipperListsRecursive(root, node2);
+        implementation.displayIterator(zipper1);
+
+
     }
 }
--- src/main/java/com/alamin/structy/linkedList/LinkedListImplementation.java
+++ src/main/java/com/alamin/structy/linkedList/LinkedListImplementation.java
@@ -1,5 +1,8 @@
 package com.alamin.structy.linkedList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinkedListImplementation<T> {
     public Node<T> insertAtFirst(T data, Node<T> head){
         return new Node<>(data, head);
@@ -105,4 +108,210 @@ public class LinkedListImplementation<T> {
         System.out.print("("+head.getData()+")-->");
         displayRecursively(head.getNext());
     }
+
+
+    public List<T> linkedListValuesIterative(Node<T> head){
+        List<T> values = new ArrayList<>();
+        while (head != null){
+            values.add(head.getData());
+            head  = head.getNext();
+        }
+        return values;
+    }
+    /**
+     * linkedListValuesIterative
+     * n = size of linked list
+     * Time: O(n)
+     * Space: O(n)
+     */
+    public List<T> linkedListValuesRecursive(Node<T> head){
+        List<T> values = new ArrayList<>();
+        linkedListValuesRecursive(head, values);
+        return values;
+    }
+
+    private void linkedListValuesRecursive(Node<T> head, List<T> values){
+        if (head == null) {
+            return;
+        }
+        values.add(head.getData());
+        linkedListValuesRecursive(head.getNext(), values);
+    }
+
+    /**
+     * linkedListValuesRecursive
+     * n = size of linked list
+     * Time: O(n)
+     * Space: O(n)
+     */
+
+    public int sumListIterative(Node<T> head){
+        try {
+            int sum = 0;
+            while (head != null){
+                sum = sum + Integer.parseInt(String.valueOf(head.getData()));
+                head  = head.getNext();
+            }
+            return sum;
+        }catch (Exception e){
+           e.printStackTrace();
+        }
+
+        return Integer.MIN_VALUE;
+    }
+
+    /**
+     * sumListIterative
+     * n = size of linked list
+     * Time: O(n)
+     * Space: O(1)
+     */
+    public int sumListRecursive(Node<T> head){
+        try {
+            if (head == null){
+                return 0;
+            }
+            return Integer.parseInt(String.valueOf(head.getData())) + sumListRecursive(head.getNext());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return Integer.MIN_VALUE;
+    }
+
+    /**
+     * sumListRecursive
+     * n = size of linked list
+     * Time: O(n)
+     * Space: O(n)
+     */
+
+    public boolean linkedListFindIterative(Node<T> head, T target){
+        Node<T> current = head;
+        while (current != null) {
+            if (target == current.getData()) {
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+    /**
+     * linkedListFindIterative
+     * n = size of linked list
+     * Time: O(n)
+     * Space: O(1)
+     */
+    public boolean linkedListFindRecursive(Node<T> head, T target){
+        if (head == null) {
+            return false;
+        }
+        if (head.getData() == target) {
+            return true;
+        }
+        return linkedListFindRecursive(head.getNext(), target);
+    }
+    /**
+     * linkedListFindRecursive
+     * n = size of linked list
+     * Time: O(n)
+     * Space: O(n)
+     */
+
+    public Node<T> reverseListIterative(Node<T> head) {
+        Node<T> previous = null;
+        Node<T> current = head;
+        while (current != null) {
+            Node<T> next = current.getNext();
+            current.setNext(previous);
+            previous = current;
+            current = next;
+        }
+        return previous;
+    }
+
+    /**
+     * reverseListIterative
+     *  n = size of linked list
+     * Time: O(n)
+     * Space: O(1)
+     */
+    public Node<T> reverseListRecursive(Node<T> head) {
+        return reverseListRecursive(head, null);
+    }
+    public Node<T> reverseListRecursive(Node<T> head, Node<T> prev) {
+        if (head == null) {
+            return prev;
+        }
+        Node<T> next = head.getNext();
+        head.setNext(prev);
+        return reverseListRecursive(next, head);
+    }
+    /**
+     * reverseListRecursive
+     * n = size of linked list
+     * Time: O(n)
+     * Space: O(n)
+     */
+
+    public Node<T> zipperListsIterative(Node<T> head1, Node<T> head2) {
+        Node<T> head = head1;
+        Node<T> tail = head;
+        Node<T> current1 = head1.getNext();
+        Node<T> current2 = head2;
+
+        int count = 0;
+        while (current1 != null && current2 != null) {
+            if (count % 2 == 0) {
+                tail.setNext(current2);
+                current2 = current2.getNext();
+            } else {
+                tail.setNext(current1);
+                current1 = current1.getNext();
+            }
+            tail = tail.getNext();
+            count += 1;
+        }
+
+        if (current1 != null) {
+            tail.setNext(current1);
+        }
+        if (current2 != null) {
+            tail.setNext(current2);
+        }
+
+        return head;
+    }
+    /**
+     * zipperListsIterative
+     * n = size of linked list
+     * Time: O(n)
+     * Space: O(1)
+     */
+
+    public Node<T> zipperListsRecursive(Node<T> head1, Node<T> head2) {
+        return zipperListsRecursive(head1, head2, 0);
+    }
+    private Node<T> zipperListsRecursive(Node<T> head1, Node<T> head2, int count) {
+        if (head1 == null) {
+            return head2;
+        }
+        if (head2 == null) {
+            return head1;
+        }
+
+        if (count % 2 == 0) {
+            head1.setNext(zipperListsRecursive(head1.getNext(), head2, count + 1));
+            return head1;
+        } else {
+            head2.setNext(zipperListsRecursive(head1, head2.getNext(), count + 1)); ;
+            return head2;
+        }
+    }
+    /**
+     * zipperListsRecursive
+     * n = size of linked list
+     * Time: O(n)
+     * Space: O(n)
+     */
 }
