daec75f2f5c9a68a5c48dc6a7f81125c9c4b0d8a
AlaminRonya <hprony12@gmail.com>
2023-09-23 16:43:59 +0600

    Merge two list using linked-list

--- src/main/java/com/alamin/structy/linkedList/LinkedListAdvanceTopic.java
+++ src/main/java/com/alamin/structy/linkedList/LinkedListAdvanceTopic.java
@@ -32,5 +32,37 @@ public class LinkedListAdvanceTopic<T> {
         return A;
     }
 
+    public Node<Integer> mergeTwo(Node<Integer> list1, Node<Integer> list2){
+        if (list1 == null){
+            return list2;
+        }
+        if (list2 == null){
+            return list1;
+        }
+        Node<Integer> curr = new Node<>(10);
+        Node<Integer> head = curr;
+
+        while (list1 != null && list2 != null){
+            if (list1.getData() <= list2.getData()){
+                curr.setNext(list1);
+                curr = curr.getNext();
+                list1 = list1.getNext();
+            }else if (list1.getData() > list2.getData()){
+                curr.setNext(list2);
+                curr = curr.getNext();
+                list2 = list2.getNext();
+            }
+        }
+
+        if (list1 != null){
+            curr.setNext(list1);
+        }
+        if (list2 != null){
+            curr.setNext(list2);
+        }
+
+        return head.getNext();
+    }
+
 
 }
--- src/main/java/com/alamin/structy/linkedList/MainAdvance.java
+++ src/main/java/com/alamin/structy/linkedList/MainAdvance.java
@@ -14,6 +14,23 @@ public class MainAdvance {
         LinkedListAdvanceTopic<Integer> topic = new LinkedListAdvanceTopic<>();
         var oddEven = topic.OddEvenList(root);
         implementation.displayIterator(oddEven);
+        Node<Integer> list1 = null;
+        list1 = implementation.insertAtFirst(60, null);
+        list1 = implementation.insertAtFirst(50, list1);
+        list1 = implementation.insertAtFirst(40, list1);
+        list1 = implementation.insertAtFirst(30, list1);
+        list1 = implementation.insertAtFirst(20, list1);
+        list1 = implementation.insertAtFirst(10, list1);
+        Node<Integer> list2 = null;
+        list2 = implementation.insertAtFirst(60, null);
+        list2 = implementation.insertAtFirst(50, list2);
+        list2 = implementation.insertAtFirst(40, list2);
+        list2 = implementation.insertAtFirst(30, list2);
+        list2 = implementation.insertAtFirst(20, list2);
+        list2 = implementation.insertAtFirst(10, list2);
+
+        Node<Integer> mergeTwo = topic.mergeTwo(list1, list2);
+        implementation.displayIterator(mergeTwo);
 
     }
 }
