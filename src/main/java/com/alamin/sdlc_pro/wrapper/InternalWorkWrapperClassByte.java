fd2f72b523940daeeb5ee6a871269a2fe8e69221
AlaminRonya <hprony12@gmail.com>
2023-10-01 21:52:12 +0600

    Wrapper class Byte & Short

---
+++ src/main/java/com/alamin/sdlc_pro/wrapper/InternalWorkWrapperClassByte.java
@@ -0,0 +1,26 @@
+package com.alamin.sdlc_pro.wrapper;
+
+public class InternalWorkWrapperClassByte {
+    public static void main(String[] args) {
+        // TODO:  Byte.valueOf(
+        //     string ,
+        //     radix = (binary\octal\decimal\hexadecimal\((0 to 9)+(A to Z))=36)
+        //  );
+
+        int binary = 2;
+        int octal = 8;
+        int decimal = 10;
+        int hexadecimal = 16;
+        int a_z_0_9 = 36;
+        System.out.println(Byte.valueOf("1010", binary));
+        System.out.println(Byte.valueOf("137", octal));
+        System.out.println(Byte.valueOf("97", decimal));
+        System.out.println(Byte.valueOf("F", hexadecimal));
+        System.out.println(Byte.valueOf("Z", a_z_0_9));
+        System.out.println("Decode Value");
+//        System.out.println(Byte.decode("0b101"));
+        System.out.println(Byte.decode("065"));
+        System.out.println(Byte.decode("89"));
+        System.out.println(Byte.decode("0xF"));
+    }
+}
---
+++ src/main/java/com/alamin/sdlc_pro/wrapper/WrapperClassInternalWork.java
@@ -0,0 +1,18 @@
+package com.alamin.sdlc_pro.wrapper;
+
+public class WrapperClassInternalWork {
+    public static void main(String[] args) {
+//        Short b1 = new Short((short) 10);
+//        Short b2 = new Short((short)10);
+        Short b3 = Short.valueOf((short) 10);
+        Short b4 = Short.valueOf((short) 10);
+        Short b5 = (short) 128;
+        Short b6 = (short) 128;
+
+//        System.out.println("new Short((short) 10): "+(b1 == b2));
+        System.out.println("Short.valueOf((short) 10): "+(b3 == b4));
+        System.out.println("(short) 128 or Short.valueOf((short) 128): "+(b5 == b6));
+        System.out.println("Cache Range: -128 to 127 than create new object.");
+        System.out.println("Short b6 = (short) 128 :=> new Short((short)128)");
+    }
+}
