5f86384f5e4a5e18ef308f7ce85a5f96e31865f9
AlaminRonya <hprony12@gmail.com>
2023-12-19 22:23:50 +0600

    Executor service example 2 & 3

--- src/main/java/com/alamin/thread/executor_service/ExecutorServiceExample1.java
+++ src/main/java/com/alamin/thread/executor_service/ExecutorServiceExample1.java
@@ -1,11 +1,11 @@
 package com.alamin.thread.executor_service;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class ExecutorServiceExample1 {
     public static void main(String[] args) {
         example1();
+        example2();
 
     }
 
@@ -16,6 +16,32 @@ public class ExecutorServiceExample1 {
         executorService.execute(newRunnable("Task 3"));
         executorService.execute(newRunnable("Task 4"));
         executorService.shutdown();
+    }
+    public static void example2(){
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        //ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(newRunnable("Task 1.1"));
+        executorService.execute(newRunnable("Task 1.2"));
+        executorService.execute(newRunnable("Task 1.3"));
+        executorService.execute(newRunnable("Task 1.4"));
+        executorService.shutdown();
+    }
+    public static void example3(){
+        int corePoolSize = 10;
+        int maxPoolSize = 20;
+        long keepAliveTime = 3000;
+        ExecutorService threadPoolExecutor = new ThreadPoolExecutor(
+                corePoolSize,
+                maxPoolSize,
+                keepAliveTime,
+                TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<>(128)
+        );
+        threadPoolExecutor = Executors.newFixedThreadPool(3);
+        ExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(corePoolSize);
+        scheduledExecutorService = Executors.newFixedThreadPool(10);
+
+
     }
     private static Runnable newRunnable(String s) {
         return () -> {
