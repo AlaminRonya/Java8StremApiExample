57754647de1bd28f75eab0b9283033e62c9f0d8a
AlaminRonya <hprony12@gmail.com>
2023-12-26 21:30:37 +0600

    Lock Basic Implementation

---
+++ src/main/java/com/alamin/thread/lazy_programmer/lock/ReentrantDemo.java
@@ -0,0 +1,28 @@
+package com.alamin.thread.lazy_programmer.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantDemo {
+    private static final ReentrantLock resource = new ReentrantLock();
+
+    public static void main(String[] args) {
+        // Acquiring Lock
+        resource.lock();
+        try {
+            System.out.println("Inside Outer Critical Section");
+            // Perform some operation
+
+            // Reentrant Behavior
+            // Start of Nested Critical Section
+            resource.lock();
+            try {
+
+            }finally {
+                resource.unlock();
+            }
+
+        }finally {
+            resource.unlock();
+        }
+    }
+}
