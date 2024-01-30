542fb13c09300437eda2f51db680f5dec5bd5abf
AlaminRonya <hprony12@gmail.com>
2024-01-30 23:04:12 +0600

    MCQ OOPs for static

---
+++ src/main/java/com/alamin/oops/MCQ.java
@@ -0,0 +1,17 @@
+package com.alamin.oops;
+class A{
+    public static void print(){
+        System.out.println("A");
+    }
+}
+class B extends A{
+    public static void print(){
+        System.out.println("B");
+    }
+}
+public class MCQ {
+    public static void main(String[] args) {
+        A a =  new B();
+        a.print();
+    }
+}
