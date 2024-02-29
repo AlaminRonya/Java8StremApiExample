e59d6d2e342f5ee45e8be3e93b0358de937f292d
AlaminRonya <hprony12@gmail.com>
2024-02-29 21:24:18 +0600

    Basic

---
+++ src/main/java/com/alamin/design_pattern/Helper.java
@@ -0,0 +1,7 @@
+package com.alamin.design_pattern;
+
+public class Helper {
+    public void performTask(){
+        System.out.println("Perform Task");
+    }
+}
---
+++ src/main/java/com/alamin/design_pattern/Main.java
@@ -0,0 +1,7 @@
+package com.alamin.design_pattern;
+public class Main {
+    public static void main(String[] args) {
+        Helper helper = new Helper();
+        helper.performTask();
+    }
+}
